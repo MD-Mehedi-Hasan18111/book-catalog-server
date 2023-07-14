c255f490c0f029177d20a4c308aa699e762c8777
MD Mehedi Hasan <md@bursement.com>
2023-07-15 01:15:26 +0600

    Server made and APIs made of authentication and Books CRUD

---
+++ server.js
@@ -0,0 +1,258 @@
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yai2s.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    const db = client.db("book-catalog");
+    const booksCollection = db.collection("books");
+    const usersCollection = db.collection("users");
+
+    // Authentication APIs Start
+    app.post("/auth/signup", async (req, res) => {
+      const userData = req.body;
+
+      // find user is exist or not
+      const isExistUser = await usersCollection.findOne({
+        email: userData.email,
+      });
+      if (isExistUser) {
+        return res.status(400).send({
+          message: "This email already exist!",
+        });
+      } else {
+        // hashing password
+        const hashedPassword = await bcrypt.hash(userData.password, 12);
+
+        userData.password = hashedPassword;
+
+        const result = await usersCollection.insertOne(userData);
+        if (result.acknowledged == true) {
+          return res.status(200).send({
+            message: "User sign up successfully!",
+          });
+        } else {
+          return res.status(400).send({
+            message: "Sign Up Failed!",
+          });
+        }
+      }
+    });
+
+    app.post("/auth/login", async (req, res) => {
+      const userData = req.body;
+      const isAvailableUser = await usersCollection.findOne({
+        email: userData.email,
+      });
+      if (!isAvailableUser) {
+        return res.status(400).send({
+          message: "This email does not exist!",
+        });
+      } else {
+        const isPasswordMatched = await bcrypt.compare(
+          userData.password,
+          isAvailableUser.password
+        );
+        if (!isPasswordMatched) {
+          return res.status(400).send({
+            message: "Incorrect Password!",
+          });
+        } else {
+          const accessToken = await jwt.sign(
+            { email: isAvailableUser.email },
+            "tokenSecret",
+            { expiresIn: "30d" }
+          );
+          return res.status(200).send({
+            message: "Login successfully!",
+            token: accessToken,
+          });
+        }
+      }
+    });
+    // Authentication APIs End
+
+    // Books APIs Start
+    app.get("/books/all-books", async (req, res) => {
+      const books = await booksCollection.find({}).toArray();
+      return res.status(200).send({
+        message: "All Books retrieved successfully!",
+        books: books,
+      });
+    });
+
+    app.get("/books", async (req, res) => {
+      const { search, genre, publicationDate } = req.query;
+
+      // Prepare the filter conditions
+      const filter = {};
+
+      if (search) {
+        // Use search for title, author name, and genre
+        filter.$or = [
+          { title: { $regex: search, $options: "i" } },
+          { author: { $regex: search, $options: "i" } },
+          { genre: { $regex: search, $options: "i" } },
+        ];
+      }
+
+      if (genre) {
+        // Filter by genre
+        filter.genre = genre;
+      }
+
+      if (publicationDate) {
+        // Extract the year from the publication date and filter by year
+        const startYear = new Date(publicationDate, 0, 1);
+        const endYear = new Date(publicationDate, 11, 31);
+        filter.publicationDate = { $gte: startYear, $lte: endYear };
+      }
+
+      const books = await booksCollection.find(filter).toArray();
+      return res.status(200).send({
+        message: "Books retrieved successfully!",
+        books: books,
+      });
+    });
+
+    app.get("/books/:id", async (req, res) => {
+      const bookId = req.params.id;
+      const book = await booksCollection.findOne({ _id: bookId });
+
+      if (book) {
+        return res.status(200).send({
+          message: "Book details retrieved successfully!",
+          book: book,
+        });
+      } else {
+        return res.status(404).send({
+          message: "Book not found",
+        });
+      }
+    });
+
+    app.post("/books/add-book", async (req, res) => {
+      const authorizeToken = req.headers.authorization;
+      if (!authorizeToken) {
+        return res.status(400).send({
+          message: "Authorization not provided",
+        });
+      } else {
+        const verifiedUser = await jwt.verify(authorizeToken, "tokenSecret");
+        if (!verifiedUser) {
+          return res.status(400).send({
+            message: "You are not authorized",
+          });
+        } else {
+          const bookData = req.body;
+          const result = await booksCollection.insertOne(bookData);
+          if (result.acknowledged == true) {
+            return res.status(200).send({
+              message: "Book added successfully!",
+              book: bookData,
+            });
+          } else {
+            return res.status(400).send({
+              message: "Book added failed!",
+            });
+          }
+        }
+      }
+    });
+
+    app.put("/books/update-book/:id", async (req, res) => {
+      const authorizeToken = req.headers.authorization;
+      if (!authorizeToken) {
+        return res.status(400).send({
+          message: "Authorization not provided",
+        });
+      } else {
+        const verifiedUser = await jwt.verify(authorizeToken, "tokenSecret");
+        if (!verifiedUser) {
+          return res.status(400).send({
+            message: "You are not authorized",
+          });
+        } else {
+          const bookId = req.params.id;
+          const updatedBookData = req.body;
+
+          const result = await booksCollection.updateOne(
+            { _id: bookId },
+            { $set: updatedBookData }
+          );
+
+          if (result.matchedCount > 0) {
+            return res.status(200).send({
+              message: "Book updated successfully!",
+              book: updatedBookData,
+            });
+          } else {
+            return res.status(404).send({
+              message: "Book not found",
+            });
+          }
+        }
+      }
+    });
+
+    app.delete("/books/:id", async (req, res) => {
+      const authorizeToken = req.headers.authorization;
+      if (!authorizeToken) {
+        return res.status(400).send({
+          message: "Authorization not provided",
+        });
+      } else {
+        const verifiedUser = await jwt.verify(authorizeToken, "tokenSecret");
+        if (!verifiedUser) {
+          return res.status(400).send({
+            message: "You are not authorized",
+          });
+        } else {
+          const bookId = req.params.id;
+
+          const result = await booksCollection.deleteOne({ _id: bookId });
+
+          if (result.deletedCount > 0) {
+            return res.status(200).send({
+              message: "Book deleted successfully!",
+            });
+          } else {
+            return res.status(404).send({
+              message: "Book not found",
+            });
+          }
+        }
+      }
+    });
+    // Books APIs End
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+run().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
